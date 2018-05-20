c864b2e221aac078bb32e0a3dc885e85d39f6d1b
unknown <zxtinkerxz1@gmail.com>
2018-05-20 09:43:10 -0700

    xy ly video bang viec gui anh lien tuc

--- public/js/myjs.js
+++ public/js/myjs.js
@@ -6,10 +6,15 @@ $("#submit").click(function(){
 		alert("bạn chưa nhập tên")
 	}
 	else{
-		socket = io("https://webrtcbythaicute.herokuapp.com/");
-		
+
+		socket = io("http://localhost:3000/");
 		$("#ten").text(nameuser);
+		$("#sub").val(nameuser);
 		socket.emit("tenuser",nameuser);
+		socket.on('play stream', function (image){
+				document.getElementById('streaming').src = image;
+				
+				});
 
 		socket.on("server gui kq dk", function(data){
 			if(data==0){
@@ -35,10 +40,24 @@ $("#submit").click(function(){
 			if(x!="")$("#table").html(x);
 			
 		})
+		socket.on("disconnectvideo", function(data){
+			$("#createroom").css("display","block")
+			
+		})
+		socket.on("disablebutton", function(data){
+			$("#createroom").css("display","none")
+			
+		})
 
 	}
 	
 });
+
+$("#createroom").click(function(){
+	socket.emit('create');
+
+});
+
 
 
 
@@ -49,3 +68,59 @@ $("#logout").click(function(){
 	socket.disconnect();
 });
 
+
+(function (d, w, n, io){
+			'use strict'
+
+			var io = io(),
+				startCamera = false,
+				video = d.querySelector('#video'),
+				canvas = d.querySelector('#canvas'),
+				context = canvas.getContext('2d')
+
+			n.streaming = (
+				n.getUserMedia ||
+				n.webkitGetUserMedia ||
+				n.mozGetUserMedia ||
+				n.msGetUserMedia
+			)
+
+			n.streaming({
+				video : true,
+				audio : false
+			}, function (stream){
+				startCamera = true
+				video.src = w.URL.createObjectURL(stream)
+			}, function (err){
+				alert('error al acceder a la camara web: ' + err)
+			})
+
+			w.playVideo = (function (cb){
+				return w.requestAnimationFrame ||
+					w.webkitRequestAnimationFrame ||
+					w.mozRequestAnimationFrame ||
+					w.msRequestAnimationFrame ||
+					function (cb) {
+						w.setTimeout(cb, 1000/100)
+					}
+			})()
+
+			function streamVideo(context, canvas, video)
+			{
+				var outputStream = canvas.toDataURL('image/jpeg', .2)
+				context.drawImage(video, 0, 0)
+
+				if(startCamera)
+					io.emit('streaming', outputStream)
+
+				playVideo(function (){
+					streamVideo(context, canvas, video)
+				})
+			}
+
+			w.addEventListener('load', function (){
+				video.autoplay = true
+				video.style.display = 'none'
+				streamVideo(context, canvas, video)
+			})
+		})(document, window, navigator, io)
